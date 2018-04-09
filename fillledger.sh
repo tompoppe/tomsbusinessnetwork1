3ac5b6a6259f33aa915d087f0ba792496e6ac7c3
TP <it@tompoppe.be>
2018-04-09 15:23:13 +0200

    wip
    wip

--- fillledger.sh
+++ fillledger.sh
@@ -1,19 +1,4 @@
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
-   "$class": "org.tompoppe.hyperledger.AccountOwner", \ 
-   "BIC": "KREDBEBB" \ 
- }' 'http://localhost:3000/api/AccountOwner'
-
- curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
-   "$class": "org.tompoppe.hyperledger.AccountServicer", \ 
-   "BIC": "CITIUS33" \ 
- }' 'http://localhost:3000/api/AccountServicer'
-
- curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
-   "$class": "org.tompoppe.hyperledger.Account", \ 
-   "IBAN": "IBAN1234567", \ 
-   "description": "My First Account", \ 
-   "balance": 0, \ 
-   "owner": "KREDBEBB", \ 
-   "servicer": "CITIUS33" \ 
- }' 'http://localhost:3000/api/Account'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.tompoppe.hyperledger.AccountOwner", "BIC": "KREDBEBB" }' 'http://localhost:3000/api/AccountOwner'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.tompoppe.hyperledger.AccountServicer", "BIC": "CITIUS33"  }' 'http://localhost:3000/api/AccountServicer'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.tompoppe.hyperledger.Account", "IBAN": "IBAN1234567", "description": "My First Account",  "balance": 0,  "owner": "KREDBEBB", "servicer": "CITIUS33" }' 'http://localhost:3000/api/Account'
  
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 composer archive create -t dir -n .
-composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName tomsnetwork1
-composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile tomsnetwork1@0.0.1.bna --file networkadmin.card
+composer network install -c PeerAdmin@hlfv1 -a tomsnetwork1@0.0.1.bna
+composer network start --networkName tomsnetwork1 --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@tomsnetwork1

3a3068bc565d0c01239090ee12d51f66a241de4e
TP <it@tompoppe.be>
2018-05-27 11:17:52 +0200

    clean

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
 composer archive create -t dir -n .
-composer network install -c PeerAdmin@hlfv1 -a tomsnetwork1@0.0.1.bna
-composer network start --networkName tomsnetwork1 --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network install -c PeerAdmin@hlfv1 -a tomsnetwork1@0.0.2.bna
+composer network start --networkName tomsnetwork1 --networkVersion 0.0.2 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@tomsnetwork1
 composer-playground
